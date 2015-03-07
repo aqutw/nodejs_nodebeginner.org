d037ae63342121796a79ba9d488996255312888b
aqutw <aquarian.ex@gmail.com>
2015-03-07 12:27:10 +0800

    POC blocking

--- blocking-and-non-blocking/router.js
+++ blocking-and-non-blocking/router.js
@@ -2,7 +2,7 @@ function route(handle, pathname) {
   console.log("About to route a request for " + pathname);
 
   if (typeof handle[pathname] === 'function') {
-    handle[pathname]();
+    console.log( handle[pathname]() );
   } else {
     console.log("No request handler found for " + pathname);
   }

da32e4baed55bc5f1bf3ae7c02eb93662358c207
aqutw <aquarian.ex@gmail.com>
2015-03-07 13:24:38 +0800

    Add postData parameter into route Function

--- handling-post-requests/router.js
+++ handling-post-requests/router.js
@@ -1,9 +1,9 @@
-function route(handle, pathname, response) {
+function route(handle, pathname, response, postData) { // route will accept many many objects time by time.
   console.log("About to route a request for " + pathname);
 
   if (typeof handle[pathname] === 'function') {
     // console.log( handle[pathname]() );
-    handle[pathname](response);
+    handle[pathname](response, postData);
   } else {
     console.log("No request handler found for " + pathname);
 
